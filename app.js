1d5e68a7848e45bf429a03e43e6ddeadad2e4332
Your Name <shellzzonez@gmail.com>
2020-06-09 17:34:24 +0100

    finished dice

--- app.js
+++ app.js
@@ -21,39 +21,28 @@ var roundScore = 0;
 var activePlayer = 1;
 
 
-// Will choose a random number between 1 - 6
-
-dice = Math.floor(Math.random() * 6) + 1;
-
-
-// SETTING
-
-document.querySelector('#current-' + activePlayer). textContent = dice;
-
-// READING
-var x = document.querySelector('#score-0').textContent; 
-console.log(x);
-
-// Query Selector used for SETTING and READING 
-
-// . style . what style = changed STYLE
-
 document.querySelector('.dice').style.display = 'none';
-
-// Add event Listener () GETS ADDED TO EXECUTION STACK END in message queue (scroll event AND click event)
-
 
 
 document.querySelector('.btn-roll').addEventListener('click', function(){
+
     // 1. Random number
     var dice = Math.floor(Math.random() * 6) + 1;
 
 
     // 2. Display the result
-    document.querySelector('.dice').style.display = 'block';
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice + '.png'; 
 
     // 3. Update the round score IF the rolled number was not a 1
 
 });
 
 
+
+
+// document.querySelector('#current-' + activePlayer). textContent = dice;
+//document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
+// var x = document.querySelector('#score-0').textContent; 
+// console.log(x);
