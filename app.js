8bc1b1c226192d544785569d55b0e1bf3c92e273
Your Name <shellzzonez@gmail.com>
2020-07-23 18:01:53 +0100

    added winning score input

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var scores, roundScore, activePlayer, gamePlaying;
 
 init();
 
-var lastDice;
+var lastDice
 
 
 function init() {
@@ -76,8 +76,18 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
         // Update the UI
         document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
 
+
+        var input = document.querySelector('.final-score').value;
+        var winningScore;
+        if(input) {
+            var winningScore = input;
+
+        }else {
+            winningScore = 100;
+        }
+
         // Check if player won the game
-        if (scores[activePlayer] >= 100) {
+        if (scores[activePlayer] >= winningScore) {
             document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
             document.querySelector('.dice').style.display = 'none';
             document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
